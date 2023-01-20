b7b11e9484a49e7bab3341ee2861756c2c8d62f2
BCarta7 <cannolidude@msn.com>
2023-01-20 13:31:31 -0800

    basic index endpoint and test complete

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,2 +1,22 @@
 class MetricsController < ApplicationController
+
+    def index
+        @metrics = Metric.all
+        render json: @metrics
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy 
+    end
+
+    private
+    def metric_params
+        params.require(:metric).permit(:location, :name_of_run, :run_difficulty, :elevation_change, :average_speed, :number_of_runs, :comment, :wins, :user_id)
+    end
+    
 end
--- spec/requests/metrics_spec.rb
+++ spec/requests/metrics_spec.rb
@@ -2,6 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "Metrics", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of metrics" do
+
+    User.create(
+      email: "test@test.com",
+      password: "password",
+      password_confirmation: "password",
+      username: "testymctestpants"
+    ) 
+    user = User.last
+    Metric.create(
+      location:"bigbear", name_of_run:"bigrun", run_difficulty:3, elevation_change:4000, average_speed:30, number_of_runs:3, comment:"this slapped", wins:0, user_id:user.id
+    )
+      get '/metrics'
+
+      metric = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(metric.length).to eq 1
+    end
   end
+
 end
+
