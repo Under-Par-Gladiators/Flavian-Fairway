99e1c408498461d17a3271726412a57492983af7
BCarta7 <cannolidude@msn.com>
2023-01-20 11:13:29 -0800

    basic validations and testing complete

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,3 +1,4 @@
 class Metric < ApplicationRecord
     belongs_to :user
+    validates :location, :name_of_run, :run_difficulty, :elevation_change, :average_speed, :number_of_runs, :comment, :wins, :user_id, presence: true
 end
