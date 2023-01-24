4c5b56a18e1c051d531b77ec26fc542e15e02a43
Mannyplg <pettigrew.i@gmail.com>
2023-01-24 14:57:52 -0800

    metricEdit and testing completed

---
+++ app/javascript/components/pages/MetricsEdit.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import MetricsEdit from "./metricsedit";
+import { BrowserRouter } from "react-router-dom";
+
+describe("<MetricsEdit />", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <MetricsEdit />
+      </BrowserRouter>
+    );
+  });
+
+  it("Has a form with correct labels", () => {
+    const formLocation = screen.getByText("Location");
+    expect(formLocation.getAttribute("For")).toEqual("location");
+
+    const formNameOfRun = screen.getByText("Name of Run");
+    expect(formNameOfRun.getAttribute("For")).toEqual("name_of_run");
+
+    const formRunDifficulty = screen.getByText("Run Difficulty");
+    expect(formRunDifficulty.getAttribute("For")).toEqual("run_difficulty");
+
+    const formElevationChange = screen.getByText("Elevation Change");
+    expect(formElevationChange.getAttribute("For")).toEqual("elevation_change");
+
+    const formAverageSpeed = screen.getByText("Average Speed");
+    expect(formAverageSpeed.getAttribute("For")).toEqual("average_speed");
+
+    const formNumberOfRuns = screen.getByText("Number of Runs");
+    expect(formNumberOfRuns.getAttribute("For")).toEqual("number_of_runs");
+
+    const formComment = screen.getByText("Comment");
+    expect(formComment.getAttribute("For")).toEqual("comment");
+
+    const formWins = screen.getByText("Wins");
+    expect(formWins.getAttribute("For")).toEqual("wins");
+  });
+});
